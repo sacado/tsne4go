9cfe917ee293d128940e1ade388e3a76e1495b5d
Fabien Delorme <fabien.delorme@gmail.com>
2015-02-04 16:42:59 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tsne4go
+This is tsne4go, an implementation of the tSNE algorithm in Go.
+
+This library is strongly inspired from tsnejs, an equivalent library for javascript.
---
+++ distancer.go
@@ -0,0 +1,23 @@
+package tsne
+
+// Distancer describes a collection of points from which a distance can be computed
+type Distancer interface {
+	Len() int                  // Length of the collection
+	Distance(i, j int) float64 // Distance between items i and j of the collection
+}
+
+// A Distancer implemented for vectors of float64
+type VectorDistancer [][]float64
+
+func (vd VectorDistancer) Len() int { return len(vd) }
+
+func (vd VectorDistancer) Distance(i, j int) float64 {
+	vi := vd[i]
+	vj := vd[j]
+	dist := 0.0
+	for k, vik := range vi {
+		vjk := vj[k]
+		dist += (vik - vjk) * (vik - vjk)
+	}
+	return dist
+}
